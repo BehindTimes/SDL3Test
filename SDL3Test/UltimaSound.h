e94450fea5fb02688c2c10d46dff38d6ae41457d
BehindTimes <behindtimes@gmail.com>
2025-09-22 20:25:38 -0400

    Added changes to get to compile with Fedora and Arch Linux.

--- SDL3Test/UltimaSound.h
+++ SDL3Test/UltimaSound.h
@@ -6,8 +6,13 @@
 #include <ogg/ogg.h>
 #include <vorbis/codec.h>
 #include <vorbis/vorbisfile.h>
+#ifdef __APPLE__
 #include <OpenAL/al.h>
 #include <OpenAL/alc.h>
+#else
+#include <AL/al.h>
+#include <AL/alc.h>
+#endif
 #else
 #endif
 
